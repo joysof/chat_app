fdf96c60fb57920112817e19656740ce06fa8e04
sajib hossen <sajibhossen560@gmail.com>
2025-10-17 22:19:53 +0600

    browserRouter setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
-  return <div  className=''>App</div>
+  return <>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    
+    </>
 }
 
 export default App
