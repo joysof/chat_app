40859561c72ef612e0add1c1915de7de707aa535
sajib hossen <sajibhossen560@gmail.com>
2025-10-17 22:56:05 +0600

    setup all route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
 
 const App = () => {
   return <>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/profile' element={<Profile/>}/>
     </Routes>
     
     </>
