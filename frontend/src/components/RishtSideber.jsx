430275d932a2baae53864b7b56b42c64f6d2e101
sajib hossen <sajibhossen560@gmail.com>
2025-10-18 09:48:29 +0600

    sidebar complate

--- frontend/src/components/RishtSideber.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RishtSideber = () => {
-  return (
-    <div>RishtSideber</div>
-  )
-}
-
-export default RishtSideber
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import { ChatContainer } from '../components/ChatContainer'
-import RishtSideber from '../components/RishtSideber'
+import RightSidebar from '../components/RightSidebar'
 
 const Home = () => {
 
-  const [selectedUser , setSele]
+  const [selectedUser , setSelectedUser] = useState()
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-        <div className='backdrop-blur-xl sm:px-[10%] sm:py-[5%] overflow-hidden h-[100%] grid-cols-1 relative'>
-          <Sidebar/>
-          <ChatContainer/>
-          <RishtSideber/>
+        <div className={`backdrop-blur-xl sm:px-[10%] sm:py-[5%] overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] lg:grid-cols-[1fr_2fr_1fr]':'md:grid-cols-2'}`}>
+          <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+          <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+          <RightSidebar />
         </div>
     </div>
   )
