6c0036de9cbdf32ccffb459f8d1d99bfbcf25c0e
sajib hossen <sajibhossen560@gmail.com>
2025-10-18 22:18:43 +0600

    complated rightSidebar degisen

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -4,6 +4,7 @@ import assets, { imagesDummyData } from '../assets/assets'
 const RightSidebar = ({selectedUser}) => {
   return selectedUser &&(
     <div className={`bg-[#8185b2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? 'max-md:hidden' : ''} `}>
+      {/* user data  */}
         <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'> 
           <img src={selectedUser?.profilepic || assets.avatar_icon} alt="profile photo " className='w-20 aspect-[1/1] rounded-full' />
           <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
@@ -12,7 +13,8 @@ const RightSidebar = ({selectedUser}) => {
             <p className='px-10 mx-auto'>{selectedUser.bio}</p>
         </div>
         <hr  className='border-[#ffffff50 my-2]'/>
-
+           
+           {/* photo section  */}
         <div className='px-5 text-xs '>
           <p>Media</p>
           <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
@@ -24,7 +26,7 @@ const RightSidebar = ({selectedUser}) => {
               ))
             }
           </div>
-
+             <button  className=' absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 rounded-full cursor-pointer px-20 ' >Logout</button>
         </div>
     </div>
   )
