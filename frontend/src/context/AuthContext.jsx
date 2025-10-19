bcda427a00ac5159183021633009e49d5e19f96c
sajib hossen <sajibhossen560@gmail.com>
2025-10-19 17:57:21 +0600

    logout user

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,50 +1,67 @@
-import { createContext, useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-
-
+import { createContext, useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export const AuthContext = createContext()
 
+export const AuhtProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [token, setToken] = useState(localStorage.getItem('token') || null)
+  const navigate = useNavigate()
+  const [loading, setLoding] = useState(false)
 
-export const AuhtProvider = ({children}) =>{
-    const [user , setUser] = useState(null)
-    const [token , setToken ] = useState(localStorage.getItem("token") || null)
-    const navigate = useNavigate()
-    const [loading , setLoding] = useState(false)
-
-    const backend_url = import.meta.env.VITE_BACKEND_URL
-    const login = async (email , password) =>{
-        setLoding(true)
-        try {
-            const res = await axios.post(`${backend_url}/api/v1/auth/login` , {email , password})
-            setUser(res.data.data.attributes.user)
-            setToken(res.data.data.attributes.tokens)
-            localStorage.setItem("token" , res.data.data.attributes.tokens.access.token)
-        } catch (error) {
-            console.log(error)
-        }finally{
-            setLoding(false)
-        }
+  const backend_url = import.meta.env.VITE_BACKEND_URL
+  const login = async (email, password) => {
+    setLoding(true)
+    try {
+      const res = await axios.post(`${backend_url}/api/v1/auth/login`, {
+        email,
+        password,
+      })
+      setUser(res.data.data.attributes.user)
+      setToken(res.data.data.attributes.tokens)
+      localStorage.setItem(
+        'token',
+        res.data.data.attributes.tokens.access.token
+      )
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoding(false)
     }
+  }
 
-    const register = async ( firstName , lastName, email , password , bio) =>{
-        setLoding(true)
-        try {
-            const res = await axios.post(`${backend_url}/api/v1/auth/register` , {firstName , lastName , email , password , bio})
-             const user = res.data.data.attributes.user
-            setUser(user)
-            const registeredEmail = res.data.data?.email || email
-            localStorage.setItem("email", registeredEmail?.trim().toLowerCase())
-            console.log(registeredEmail)
-            navigate("/verify-email")
-        } catch (error) {
-            console.log(error)
-        }
+  const register = async (firstName, lastName, email, password, bio) => {
+    setLoding(true)
+    try {
+      const res = await axios.post(`${backend_url}/api/v1/auth/register`, {
+        firstName,
+        lastName,
+        email,
+        password,
+        bio,
+      })
+      const user = res.data.data.attributes.user
+      setUser(user)
+      const registeredEmail = res.data.data?.email || email
+      localStorage.setItem('email', registeredEmail?.trim().toLowerCase())
+      console.log(registeredEmail)
+      navigate('/verify-email')
+    } catch (error) {
+      console.log(error)
     }
-    return (
-        <AuthContext.Provider value={{user , token , loading ,register , login , backend_url}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  }
+  const logout = async () =>{
+    setUser(null)
+    setToken(null)
+    localStorage.removeItem("token")
+    navigate('/login')
+  }
+  return (
+    <AuthContext.Provider
+      value={{ user, token, loading, register, login, backend_url , logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
 }
