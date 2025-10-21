475efde764b06f8e1b599a922f8c3547f2fd9998
sajib hossen <sajibhossen560@gmail.com>
2025-10-21 14:52:59 +0600

    socket setup

--- frontend/src/context/MessageContaxt.jsx
+++ frontend/src/context/MessageContaxt.jsx
@@ -1,34 +1,53 @@
-import { createContext, useContext, useState } from "react";
-import { AuthContext } from "./AuthContext";
-import axios from "axios";
- export const MessageContext = createContext()
+import { createContext, useEffect, useState } from 'react'
+import { AuthContext } from './AuthContext'
+import axios from 'axios'
+import { io } from 'socket.io-client'
+import { toast } from 'react-toastify'
 
- export const MessageProvider = ({children }) =>{
-    // const {user} = useContext(AuthContext)
-    const [message , setMessage] = useState([])
-    const [loading , setLoding] = useState(false)
+export const MessageContext = createContext()
 
+export const MessageProvider = ({ children, user }) => {
+  const [message, setMessage] = useState([])
+  const [loading, setLoding] = useState(false)
+  const [socket, setSocket] = useState(null)
+  const backend_url = import.meta.env.VITE_BACKEND_URL
 
-
-      const backend_url = import.meta.env.VITE_BACKEND_URL
-
-    const getMessage = async (receiverId) =>{
-        setLoding(true)
-        try {
-            const res = await axios.get(`${backend_url}/api/message`,{
-                 params: { receiverId }
-            })
-            setMessage(res.data)
-        } catch (error) {
-            console.log(error)
-        }finally{
-            setLoding(false)
-        }
+  useEffect(() => {
+    if (user?.token) {
+      const newSocket = io(backend_url, {
+        auth: { token: user.token },
+      })
+      newSocket.on('connect', () => {
+        console.log('conected to socket server')
+      })
+      newSocket.on('new-message', (data) => {
+        setMessage((prev) => [...prev, data])
+        toast.info('new message received')
+      })
+      setSocket(newSocket)
+      return () => newSocket.disconnect()
     }
+  }, [user?.token])
 
-    return(
-        <MessageContext.Provider value={{getMessage ,message, loading}}>
-                {children}
-        </MessageContext.Provider>
-    )
+  const getMessage = async (senderId, receiverId) => {
+    setLoding(true)
+    try {
+      const res = await axios.get(`${backend_url}/api/message`, {
+        params: { senderId, receiverId },
+        headers: { Authorization: `Bearer ${user.token}` },
+      })
+      setMessage(res.data.data)
+    } catch (error) {
+        toast.error(error.response?.data?.message || "Failed to fetch messages")
+      console.log(error)
+    } finally {
+      setLoding(false)
+    }
+  }
+
+  return (
+    <MessageContext.Provider value={{ getMessage, message, loading }}>
+      {children}
+    </MessageContext.Provider>
+  )
 }
