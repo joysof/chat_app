59299d055c2c286b581abdfcf1eb8de7646e13a7
sajib hossen <sajibhossen560@gmail.com>
2025-11-03 23:56:02 +0600

    right side onli User or ofline use show

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -4,15 +4,19 @@ import { AuthContext } from '../context/AuthContext'
 import assets from '../assets/assets'
 const RightSidebar = ({selectedUser}) => {
 
-  const {message} = useContext(MessageContext)
+  const {message , onlineUsers} = useContext(MessageContext)
   const {logout} = useContext(AuthContext)
+
+  const isUserOnline = selectedUser && onlineUsers?.includes(selectedUser._id || selectedUser.id)
   return selectedUser &&(
     <div className={`bg-[#8185b2]/10 text-white w-full relative flex flex-col  overflow-y-scroll ${selectedUser ? 'max-md:hidden' : ''} `}>
       {/* user data  */}
         <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'> 
           <img src={selectedUser?.image || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full' />
           <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
-            <p className='w-2 h-2 rounded-full bg-green-400'></p>
+            {
+              isUserOnline ? <p className='w-2 h-2 rounded-full bg-green-400'></p> : <p className='w-2 h-2 rounded-full bg-stone-300'></p>
+            }
             {selectedUser.firstName} {selectedUser.lastName} </h1>
             <p className='px-10 mx-auto'>{selectedUser.bio}</p>
         </div>
